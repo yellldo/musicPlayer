bbccf6e040d633ada9e07fe6706964068e7a38b8
LIUPWAN <15097627576@163.com>
2024-01-08 14:52:23 +0800

    init

---
+++ player-base/music-crypto/src/main/java/com/music/player/crypto/MusicPlayerCryptoAutoConfiguration.java
@@ -0,0 +1,11 @@
+package com.music.player.crypto;
+
+/**
+ * ClassName : MusicPlayerCryptoAutoConfiguration<br>
+ * Description : MusicPlayerCryptoAutoConfiguration<br>
+ *
+ * @author : sj
+ * @date : 2024/1/8
+ */
+public class MusicPlayerCryptoAutoConfiguration {
+}
