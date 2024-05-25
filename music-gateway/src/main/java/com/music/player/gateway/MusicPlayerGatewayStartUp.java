313f77da48e765f9daba67833ac8fb486bdd092e
shenjia <15097627576@163.com>
2024-05-25 23:04:36 +0800

    feat:gateway

--- music-gateway/src/main/java/com/music/player/gateway/MusicPlayerGatewayStartUp.java
+++ music-gateway/src/main/java/com/music/player/gateway/MusicPlayerGatewayStartUp.java
@@ -1,5 +1,6 @@
 package com.music.player.gateway;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -11,8 +12,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @author : sj
  * @date : 2024/3/18
  */
-@EnableDiscoveryClient
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MusicPlayerGatewayStartUp {
 
     public static void main(String[] args) {
