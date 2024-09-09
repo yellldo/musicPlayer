44c9cc8dc29c2ea2fb11788f64169b1e0f58949a
shenjia <shenjia@wisecotech.com>
2024-09-09 23:43:15 +0800

    feat:fastjson换fastjson2

--- music-auth-mgt/music-auth/src/test/java/com/music/player/auth/controller/SysUserCtrlTest.java
+++ music-auth-mgt/music-auth/src/test/java/com/music/player/auth/controller/SysUserCtrlTest.java
@@ -1,6 +1,6 @@
 package com.music.player.auth.controller;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 import com.music.player.auth.dto.UserRegisterDto;
 import org.junit.Before;
 import org.junit.Test;
